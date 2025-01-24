f9c764c6899281c2705a64351836ee3f5cd56dce
meidymahardika30 <meidymahardika30@gmail.com>
2025-01-24 15:49:35 +0700

    initial

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const path = require('path');
+const app = express();
+
+// Set EJS as templating engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Set express-ejs-layouts
+app.use(expressLayouts);
+app.set('layout', 'layouts/main'); // default layout
+app.set("layout extractScripts", true);
+app.set("layout extractStyles", true);
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('pages/home', {
+        title: 'Home Page',
+        content: 'Welcome to our website'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('pages/about', {
+        title: 'About Us',
+        content: 'Learn more about our company'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
